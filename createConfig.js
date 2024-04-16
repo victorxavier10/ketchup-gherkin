718e38a14fe7873c67d160f8a24d8e8aa5954329
victorxavieratlas <victor.xavier@atlastechnol.com>
2024-04-16 17:47:49 -0300

    fix(nodemon): create nodemon generate config

--- createConfig.js
+++ createConfig.js
@@ -1,23 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const nodemonConfigPath = path.join(__dirname, 'nodemon.json');
-const configContent = JSON.stringify({
-    "watch": ["index.js", "ketchup.js", "Features/", "lib/", "bin/"],
-    "ext": "js, css, json, feature"
-}, null, 4);
+const content = `{
+  "watch": ["index.js", "ketchup.js", "Features/", "lib/", "bin/"],
+  "ext": "js, css, json, feature"
+}`;
 
-fs.access(nodemonConfigPath, fs.constants.F_OK, (err) => {
-    if (err) {
-        // Arquivo não existe, vamos criar
-        fs.writeFile(nodemonConfigPath, configContent, (writeErr) => {
-            if (writeErr) {
-                console.error('Falha ao criar o arquivo nodemon.json:', writeErr);
-            } else {
-                console.log('Arquivo nodemon.json criado com sucesso.');
-            }
-        });
-    } else {
-        console.log('Arquivo nodemon.json já existe.');
-    }
-});
+fs.writeFileSync(path.join(process.cwd(), 'nodemon.json'), content);
